0c1340113f7bfce7099eded072ad86519d344ffc
ruhiyusuf <ruhiyusuf@sfhs.com>
2023-03-17 02:31:53 +0100

    added retroreflective

--- launch_gstreamer_server_streams.sh
+++ launch_gstreamer_server_streams.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # launch gstreamer server streams
-bash /usr/local/bin/gstreamer_server.sh 10.23.67.213 &
+# bash /usr/local/bin/gstreamer_server.sh 10.23.67.213 &
+bash /usr/local/bin/gstreamer_server.sh 192.168.1.17 &
 wait
