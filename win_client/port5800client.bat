4b75472ce58b2f79ca456b3b3a3075c77699719d
bambithenambi <abhijitvnambiar@gmail.com>
2023-03-18 15:48:58 -0700

    h265 client code fixed

--- win_client/port5800client.bat
+++ win_client/port5800client.bat
@@ -1,3 +1,3 @@
 cd C:\gstreamer\1.0\x86\bin 
-gst-launch-1.0.exe -vvv -e udpsrc port=5800 ! "application/x-rtp,encoding-name=H265,payload=96" ! rtph265depay ! h265parse ! queue ! avdec_h265 ! autovideosink
+gst-launch-1.0  udpsrc port=5800 ! application/x-rtp,encoding-name=H265,payload=96 ! rtph265depay ! h265parse ! queue ! avdec_h265 ! d3d11videosink
 pause
