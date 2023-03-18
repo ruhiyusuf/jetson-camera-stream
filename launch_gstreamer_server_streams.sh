32e00c2774141d371ce33178c88d5d8d4a8c26d4
ruhiyusuf <ruhiyusuf@sfhs.com>
2023-03-18 01:43:30 +0100

    detects client ip and configure streamgit add .git add .git add .git add .!

--- launch_gstreamer_server_streams.sh
+++ launch_gstreamer_server_streams.sh
@@ -2,5 +2,7 @@
 
 # launch gstreamer server streams
 # bash /usr/local/bin/gstreamer_server.sh 10.23.67.213 &
-bash /usr/local/bin/gstreamer_server.sh 192.168.1.17 &
+CLIENT_IP=`netcat -lu -w0 5803`
+# bash /usr/local/bin/gstreamer_server.sh 10.0.0.19 &
+bash /usr/local/bin/gstreamer_server.sh $CLIENT_IP &
 wait
