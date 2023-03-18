13a6023c638fbcdb9b7c5ccb2c31262d414e6a82
bambithenambi <abhijitvnambiar@gmail.com>
2023-03-17 19:45:38 -0700

    init

--- gstreamer_server.sh
+++ gstreamer_server.sh
@@ -11,8 +11,10 @@
 
 gst-launch-1.0 \
 	nvarguscamerasrc sensor-id=0 sensor-mode=4 ! 'video/x-raw(memory:NVMM), \
-	format=NV12, width=1280, height=720' !  nvv4l2h264enc insert-sps-pps=true ! \
-	h264parse ! rtph264pay pt=96 ! udpsink host=$1 port=5800 sync=false -e \
+	width=(int)1280, height=(int)720, format=(string)NV12, \
+	framerate=(fraction)60/1' !  nvv4l2h265enc bitrate=4000000 ! \
+	h265parse ! rtph265pay mtu=1400 ! udpsink host=$1 port=5800 sync=false -e \
 	nvarguscamerasrc sensor-id=1 sensor-mode=4 ! 'video/x-raw(memory:NVMM), \
-	format=NV12, width=1280, height=720' !  nvv4l2h264enc insert-sps-pps=true ! \
-	h264parse ! rtph264pay pt=96 ! udpsink host=$1 port=5801 sync=false -e \
+	width=(int)1280, height=(int)720, format=(string)NV12, \
+	framerate=(fraction)60/1' !  nvv4l2h265enc bitrate=4000000 ! \
+	h265parse ! rtph265pay mtu=1400 ! udpsink host=$1 port=5801 sync=false -e \
