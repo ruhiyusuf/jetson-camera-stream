19b05238ee13e5e1cac40024706f7a32c5360362
ruhiyusuf <ruhiyusuf@sfhs.com>
2023-03-18 20:50:02 +0100

    added switchcam

--- launch_gstreamer_server_streams.sh
+++ launch_gstreamer_server_streams.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # launch gstreamer server streams
-CLIENT_IP=`netcat -lu -w0 5803`
+# CLIENT_IP=`netcat -lu -w0 5803`
 # bash /usr/local/bin/gstreamer_server.sh $CLIENT_IP &
 python /usr/local/bin/switchcam.py
 wait
