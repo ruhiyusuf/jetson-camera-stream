21e63e38328bb7143728db1fe421e958e4c4ca20
ruhiyusuf <ruhi.yusuf@gmail.com>
2023-03-17 22:44:18 -0700

    broadcast client ip

--- win_client/ipBroadcast.bat
+++ win_client/ipBroadcast.bat
@@ -1,3 +1,6 @@
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 echo Network IP: %NetworkIP%
+:loop
+echo %NetworkIP% | ncat -u 255.255.255.255 5803
+goto loop
 pause
